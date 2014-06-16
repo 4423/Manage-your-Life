5e26aac075e910ae11037fb7d4fdcad3fdb81212
4423 <hanamiya4423@gmail.com>
2014-06-16 21:58:46 +0900

    アイコンの表示が出来た

---
+++ Manage your Life/TimeUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Manage_your_Life
+{
+    class TimeUtility
+    {
+        /// <summary>
+        /// 使用間隔を計算
+        /// </summary>
+        /// <param name="firstActiveDate"></param>
+        /// <returns></returns>      
+        internal TimeSpan GetInterval(DateTime firstActiveDate)
+        {
+            //現在の時間から、最初のアクティブまでの時間を計算
+            return DateTime.Now - firstActiveDate;
+        }
+    }
+}
